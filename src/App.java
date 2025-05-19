08e82c59108a9e80c2acb27d798fb1297eecaa96
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-05-19 08:48:46 -0300

    Criando a classe Produto

--- src/App.java
+++ src/App.java
@@ -1,28 +1,7 @@
-import java.io.Serializable;
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-
 public class App {
 
     public static void main(String[] args) {
 
-        Pessoa pessoas[] = {
-            new Pessoa("João",
-                    LocalDate.of(2000,10,3)),
-            new Pessoa("Maria",
-                    LocalDate.of(1998, 3, 21)),
-            new Pessoa("Ana",
-                    LocalDate.of(1999,4,1))
-        };
-        Arrays.sort(pessoas);
-        System.out.println(Arrays.toString(pessoas));
-
-        Arrays.sort(pessoas,
-                Comparator.comparing(Pessoa::getNascimento)
-        );
-        System.out.println(Arrays.toString(pessoas));
     }
 
 }
