1f0dc6dd63c6f2df5fbd825daf1ebb783b834239
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-05-19 08:09:08 -0300

    Usando comarator com classe externa

--- src/App.java
+++ src/App.java
@@ -1,16 +1,28 @@
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class App {
 
     public static void main(String[] args) {
-        AreaCalculavel area1 = new Quadrado(2);
-        AreaCalculavel area2 = new Circulo(3);
 
-        System.out.println(area1.calcularArea());
-        System.out.println(area2.calcularArea());
+        Pessoa pessoas[] = {
+            new Pessoa("João",
+                    LocalDate.of(2000,10,3)),
+            new Pessoa("Maria",
+                    LocalDate.of(1998, 3, 21)),
+            new Pessoa("Ana",
+                    LocalDate.of(1999,4,1))
+        };
+        Arrays.sort(pessoas);
+        System.out.println(Arrays.toString(pessoas));
 
-        area1.desenhar();
-        area2.desenhar();
+        ComparadorPessoaNascimento comparadorPessoaNascimento
+                = new ComparadorPessoaNascimento();
+        Arrays.sort(pessoas, comparadorPessoaNascimento);
+        System.out.println(Arrays.toString(pessoas));
+
 
     }
 
