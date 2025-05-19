2e1cadb932bf7c27e44b50b23cb52dbcc55a562a
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-05-19 08:16:31 -0300

    Fazendo com classe interna

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class App {
 
@@ -18,12 +19,18 @@ public class App {
         Arrays.sort(pessoas);
         System.out.println(Arrays.toString(pessoas));
 
-        ComparadorPessoaNascimento comparadorPessoaNascimento
-                = new ComparadorPessoaNascimento();
-        Arrays.sort(pessoas, comparadorPessoaNascimento);
+        OrdenadorNascimento ordenadorNascimento
+                = new OrdenadorNascimento();
+        Arrays.sort(pessoas, ordenadorNascimento);
         System.out.println(Arrays.toString(pessoas));
+    }
 
-
+    static class OrdenadorNascimento implements Comparator<Pessoa> {
+        @Override
+        public int compare(Pessoa p1, Pessoa p2) {
+            return p1.getNascimento()
+                    .compareTo(p2.getNascimento());
+        }
     }
 
 }
